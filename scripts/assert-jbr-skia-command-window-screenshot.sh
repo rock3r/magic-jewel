9494c3abb1a896c1d29b9d2d5c6a3e0fa8007e6e
Sebastiano Poggi <poggos@gmail.com>
2026-04-29 20:30:34 +0200

    Assert command screenshot text regions

--- scripts/assert-jbr-skia-command-window-screenshot.sh
+++ scripts/assert-jbr-skia-command-window-screenshot.sh
@@ -46,6 +46,29 @@ var purple = 0
 var yellow = 0
 var orange = 0
 var white = 0
+var topText = 0
+var bottomText = 0
+
+func isDarkText(r: Int, g: Int, b: Int) -> Bool {
+    return r < 55 && g < 55 && b < 55
+}
+
+func inRect(x: Int, y: Int, left: Int, top: Int, right: Int, bottom: Int) -> Bool {
+    return x >= left && x < right && y >= top && y < bottom
+}
+
+let topTextRect = (
+    left: width / 16,
+    top: height / 12,
+    right: width * 7 / 16,
+    bottom: height / 5
+)
+let bottomTextRect = (
+    left: width / 16,
+    top: height * 7 / 10,
+    right: width / 2,
+    bottom: height * 17 / 20
+)
 
 for y in 0..<height {
     for x in 0..<width {
@@ -72,10 +95,18 @@ for y in 0..<height {
         if r > 220 && g > 220 && b > 220 {
             white += 1
         }
+        if isDarkText(r: r, g: g, b: b) {
+            if inRect(x: x, y: y, left: topTextRect.left, top: topTextRect.top, right: topTextRect.right, bottom: topTextRect.bottom) {
+                topText += 1
+            }
+            if inRect(x: x, y: y, left: bottomTextRect.left, top: bottomTextRect.top, right: bottomTextRect.right, bottom: bottomTextRect.bottom) {
+                bottomText += 1
+            }
+        }
     }
 }
 
-print("JBR_SKIA_COMMAND_SCREENSHOT_COUNTS green=\(green) blue=\(blue) purple=\(purple) yellow=\(yellow) orange=\(orange) white=\(white)")
+print("JBR_SKIA_COMMAND_SCREENSHOT_COUNTS green=\(green) blue=\(blue) purple=\(purple) yellow=\(yellow) orange=\(orange) white=\(white) topText=\(topText) bottomText=\(bottomText)")
 
 let checks: [(String, Int, Int)] = [
     ("green", green, 10000),
@@ -84,6 +115,8 @@ let checks: [(String, Int, Int)] = [
     ("yellow", yellow, 500),
     ("orange", orange, 1000),
     ("white", white, 500),
+    ("topText", topText, 900),
+    ("bottomText", bottomText, 1200),
 ]
 
 for (name, count, minimum) in checks where count < minimum {
