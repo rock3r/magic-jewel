c09d289299ec99d9bd39c7964c994668a0f28da5
Sebastiano Poggi <poggos@gmail.com>
2026-06-14 17:58:28 +0200

    Require command loaded font migration cache clears

--- scripts/jbr-skia-command-probe-suite.sh
+++ scripts/jbr-skia-command-probe-suite.sh
@@ -1465,7 +1465,9 @@ run_named_case() {
         EXPECT_MIN_JBR_FONT_DATA_DEFINES=2 \
         EXPECT_MIN_SURFACE_CHANGES=1 \
         EXPECT_SURFACE_CONTEXT_CHANGED=false \
-        EXPECT_MIN_COMMAND_CACHE_CLEARS=1
+        EXPECT_MIN_COMMAND_CACHE_CLEARS=1 \
+        EXPECT_MIN_JBR_IMAGE_CACHE_CLEARS=1 \
+        EXPECT_MIN_JBR_SCOPED_IMAGE_CACHE_CLEARS=1
       ;;
     commands-resize-native-resource-font-text)
       run_case "$1" \
@@ -1516,7 +1518,9 @@ run_named_case() {
         EXPECT_MIN_JBR_FONT_DATA_DEFINES=2 \
         EXPECT_MIN_SURFACE_CHANGES=1 \
         EXPECT_SURFACE_CONTEXT_CHANGED=true \
-        EXPECT_MIN_COMMAND_CACHE_CLEARS=1
+        EXPECT_MIN_COMMAND_CACHE_CLEARS=1 \
+        EXPECT_MIN_JBR_IMAGE_CACHE_CLEARS=1 \
+        EXPECT_MIN_JBR_SCOPED_IMAGE_CACHE_CLEARS=1
       ;;
     commands-forced-context-native-resource-font-text)
       run_case "$1" \
